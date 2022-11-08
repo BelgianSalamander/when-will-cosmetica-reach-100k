eceb8c58ee20795b2954b81337ae9fc4d62d10b9
Anatol Coen <anatol.coen@gmail.com>
2022-11-08 23:10:43 +1300

    Fixed Bug

--- script.js
+++ script.js
@@ -28,39 +28,53 @@ function formatNicely(seconds) {
     return terms.slice(0, 3).join(", ");
 }
 
+var week;
+
+function recalculate() {
+    fetch('https://api.cosmetica.cc/get/servicestats').then(function(response) {
+        response.json().then(function(data) {
+            //Get current unix time in seconds
+            const now = Math.floor(Date.now() / 1000);
+            const weekPos = (now - OFFSET) % 604800;
+            const currTS = Math.floor(weekPos / 600);
+            const tsProgress = weekPos % 600;
+
+            let tsElapsed = 0;
+            let progress = data["players"];
+
+            if (progress >= 100000) {
+                document.getElementById("elem").innerHTML = "Cosmetica has reached 100k users :D";
+                return;
+            }
+
+            needed = Math.min((600 - tsProgress) / 600, (100000 - progress) / week[currTS]);
+            progress += week[currTS] * needed;
+            tsElapsed += needed;
+
+            idx = currTS + 1;
+            idx %= week.length;
+
+            while (progress < 100000) {
+                needed = Math.min(1, (100000 - progress) / week[idx]);
+                progress += week[idx] * needed;
+                tsElapsed += needed;
+                idx++;
+                idx %= week.length;
+            } 
+
+            const timeNeeded = tsElapsed * 600;
+
+            document.getElementById("elem").innerHTML = "Cosmetica will reach 100k users in " + formatNicely(timeNeeded);
+        
+            setTimeout(recalculate, 10000);
+        });
+    });
+}
+
 //Load week.json from the server
 fetch('week.json').then(function(response) {
-    response.json().then(function(week) {
-        //Get "https://api.cosmetica.cc/get/servicestats" json
-        fetch('https://api.cosmetica.cc/get/servicestats').then(function(response) {
-            response.json().then(function(data) {
-                //Get current unix time in seconds
-                const now = Math.floor(Date.now() / 1000);
-                const weekPos = (now - OFFSET) % 604800;
-                const currTS = Math.floor(weekPos / 600);
-                const tsProgress = weekPos % 600;
-
-                let tsElapsed = 0;
-                let progress = data["players"];
-
-                progress += week[currTS] * (600 - tsProgress) / 600;
-                tsElapsed += (600 - tsProgress) / 600;
-
-                idx = currTS + 1;
-                idx %= week.length;
-
-                while (progress < 100000) {
-                    needed = Math.min(1, (100000 - progress) / week[idx]);
-                    progress += week[idx] * needed;
-                    tsElapsed += needed;
-                    idx++;
-                    idx %= week.length;
-                } 
-
-                const timeNeeded = tsElapsed * 600;
-
-                document.getElementById("elem").innerHTML = "Cosmetica will reach 100k users in " + formatNicely(timeNeeded);
-            });
-        });
+    response.json().then(function(weekData) {
+        week = weekData;
+        recalculate();
     });
 });
